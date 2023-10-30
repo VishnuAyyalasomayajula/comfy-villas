15ec1d143db9397d3c44c14741b2128fd83f9903
Vishnu Vardhan Ayyalasomayajula <ayyalasomayajula2000@gmail.com>
2023-10-30 20:45:26 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Management_SignIn from './pages/Management_SignIn';
+import Profile from './pages/Profile';
+import Resident_SignIn from './pages/Resident_SignIn';
+
+
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return 
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/ressignin' element={<Resident_SignIn />} />
+    <Route path='/profile' element={<Profile />} />
+    <Route path='/mansignin' element={<Management_SignIn />} />
+    
+  </Routes>
+  </BrowserRouter>
 }
